25a6b48cd66988a4dabc693d4390404d338260f9
CoolShivas <shivas2710cool00@gmail.com>
2024-05-13 18:30:38 +0530

    Use of Browser Router and Routes

--- react_exp_tracker02/src/App.jsx
+++ react_exp_tracker02/src/App.jsx
@@ -1,10 +1,19 @@
-
+import Header from "./components/header/Header";
+import ProductPage from "./pages/ProductPage";
+import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage";
+import { Route } from "react-router-dom/cjs/react-router-dom";
 import styles from "./App.module.css";
 
 const App = () => {
   return (
     <>
       <Header></Header>
+
+      <Route path="/homepage"> <HomePage></HomePage> </Route>
+      <Route path="/aboutpage"> <AboutPage></AboutPage> </Route>
+      <Route path="/productpage"> <ProductPage></ProductPage> </Route>
+
     </>
   )
 }
