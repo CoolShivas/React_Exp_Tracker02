1176842ef5a3b0e92b97aadb7efccfebd8385894
CoolShivas <shivas2710cool00@gmail.com>
2024-05-13 23:03:37 +0530

    Binding the form with useRef Hook

--- react_exp_tracker02/src/components/auth/AuthForm.jsx
+++ react_exp_tracker02/src/components/auth/AuthForm.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import styles from "./AuthForm.module.css";
 
 
@@ -5,10 +6,26 @@ import styles from "./AuthForm.module.css";
 
 const AuthForm = () => {
 
+    const inputEmail = useRef('');
+    const inputPassword = useRef('');
+    const inputConfirmPassword = useRef('');
+
+    const handlerOnSubmitForm = (event) => {
+        event.preventDefault();
+
+        const enteredEmail = inputEmail.current.value;
+        const enteredPassword = inputPassword.current.value;
+        const enteredConfirmPassword = inputConfirmPassword.current.value;
+
+        console.log(enteredEmail);
+        console.log(enteredPassword);
+        console.log(enteredConfirmPassword);
+    };
+
     return (
         <div className={styles.auth_form__page}>
             <div className={styles.form_container}>
-                <form>
+                <form onSubmit={handlerOnSubmitForm}>
                     <div className={styles.signup_div}>
                         <label htmlFor="signup"> SignUp </label>
                     </div>
@@ -16,11 +33,11 @@ const AuthForm = () => {
                     <div className={styles.input_fieds__div}>
 
                         <input type="email" id="signup"
-                            name="signup" placeholder="Email" required />
+                            name="signup" placeholder="Email" required ref={inputEmail} />
 
-                        <input type="password" placeholder="Password" required />
+                        <input type="password" placeholder="Password" required ref={inputPassword} />
 
-                        <input type="password" placeholder="Confirm Password" required />
+                        <input type="password" placeholder="Confirm Password" required ref={inputConfirmPassword} />
 
                     </div>
 
