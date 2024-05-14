3bceec3258445ec574b4891e1cfe9cbcf224c3e8
CoolShivas <shivas2710cool00@gmail.com>
2024-05-14 18:11:16 +0530

    Added the firebase signIn with password rest api on LogIn page

--- react_exp_tracker02/src/components/auth/LogIn.jsx
+++ react_exp_tracker02/src/components/auth/LogIn.jsx
@@ -1,17 +1,67 @@
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
 import styles from "./LogIn.module.css";
+import { useRef } from "react";
 
 const LogIn = () => {
+
+    const inputLoginEmail = useRef('');
+
+    const inputLoginPassword = useRef('');
+
+    const handlerOnSubmitLoginForm = (event) => {
+        event.preventDefault();
+
+        const enteredLoginEmail = inputLoginEmail.current.value;
+        const enteredLoginPassword = inputLoginPassword.current.value;
+
+        fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=
+        AIzaSyDH5JlMv2hpQuoT9E47PiPYHTdFE_P2Gs0`, {
+            method: "POST",
+            body: JSON.stringify({
+                email: enteredLoginEmail,
+                password: enteredLoginPassword,
+                returnSecureToken: true,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((res) => {
+            console.log(res);
+            if (res.ok) {
+                return res.json();
+            }
+            else {
+                return res.json().then((data) => {
+                    let errMsg = "Authentication Failed";
+                    if (data && data.error && data.error.message) {
+                        errMsg = data.error.message;
+                    }
+                    // throw new Eroor(errMsg);
+                    alert(errMsg);
+                })
+            }
+        }).then((response) => {
+            console.log(response);
+
+        }).catch((err) => {
+            console.log(err.message);
+            alert(err.message);
+        })
+
+        enteredLoginEmail.current.value = '';
+        enteredLoginPassword.current.value = '';
+    };
+
     return (
         <div className={styles.login_page__div}>
             <div className={styles.login_container}>
-                <form>
+                <form onSubmit={handlerOnSubmitLoginForm}>
                     <div className={styles.login_div}>
                         <h1> Login </h1>
                     </div>
                     <div className={styles.login_input__field}>
-                        <input type="email" placeholder="Email" required />
-                        <input type="password" placeholder="Password" required />
+                        <input type="email" placeholder="Email" required ref={inputLoginEmail} />
+                        <input type="password" placeholder="Password" required ref={inputLoginPassword} />
                     </div>
                     <div className={styles.login_actions}>
                         <button>
