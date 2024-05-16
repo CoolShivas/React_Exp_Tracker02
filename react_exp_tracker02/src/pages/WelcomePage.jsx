dc7cc6eb0e1b368f350c75e9ef393a4c73a8bc72
CoolShivas <shivas2710cool00@gmail.com>
2024-05-16 10:32:07 +0530

    Verify User Email ID with the help of firebase rest api

--- react_exp_tracker02/src/pages/WelcomePage.jsx
+++ react_exp_tracker02/src/pages/WelcomePage.jsx
@@ -1,8 +1,24 @@
+import axios from "axios";
 import styles from "./WelcomePage.module.css";
 import InCompleteProfilePage from "./InCompleteProfilePage";
 
 
 const WelcomePage = () => {
+
+    const handlerOnVerifyEmailId = async () => {
+        try {
+            const res = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDH5JlMv2hpQuoT9E47PiPYHTdFE_P2Gs0`, {
+                requestType: "VERIFY_EMAIL",
+                idToken: localStorage.getItem("SaveToken"),
+            })
+            console.log(res);
+            return (res);
+        } catch (error) {
+            console.log(error);
+            alert(error);
+        }
+    };
+
     return (
         <div className={styles.welcomepage_div}>
             <h3> Welcome to Expense Tracker!!! </h3>
@@ -10,7 +26,7 @@ const WelcomePage = () => {
                 <InCompleteProfilePage
                 ></InCompleteProfilePage>
             </span>
-            <button className={styles.verify_ID__btn}> Verify ID </button>
+            <button className={styles.verify_ID__btn} onClick={handlerOnVerifyEmailId}> Verify ID </button>
             <hr />
         </div>
     )
